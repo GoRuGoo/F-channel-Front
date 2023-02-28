e3725dac91d00c7d0c0b19543cfa0114dd20f348
tawakemono <2005ryuno@gmail.com>
2023-02-28 16:23:13 +0900

    feat: ボタン追加

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,27 @@
 import Link from "next/link";
-import { InlineMath, BlockMath } from 'react-katex';
+import { InlineMath } from 'react-katex';
 import 'katex/dist/katex.min.css';
 
 export default function login() {
     return (
         <div>
-            <InlineMath class="my-class">
-                \int_0^\infty x^2 dx
-            </InlineMath>
+        <p>下の問題を解答しなさい。</p>
+        <InlineMath
+            math="E = mc^2"
+            style={{ fontSize: '4em', color: 'red' }}
+        />
+        <p></p>
+        <Link href="/other-page">
+            <button>別のページに移動する1</button>
+        </Link>
+        <p></p>
+        <Link href="/other-page">
+            <button>別のページに移動する2</button>
+        </Link>
+        <p></p>
+        <Link href="/other-page">
+            <button>別のページに移動する3</button>
+        </Link>
         </div>
     );
 }
