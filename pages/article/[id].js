028dfa846e5880c9a38284e389acaba6a32af886
GoRuGoo <dachanyr8989@gmail.com>
2023-03-01 20:58:29 +0900

    feat: スレ主の投稿は表示できるようにした

--- pages/article/[id].js
+++ pages/article/[id].js
@@ -1,20 +1,26 @@
 import Header from "@/components/layouts/Header";
-import { EDGE_RUNTIME_WEBPACK } from "next/dist/shared/lib/constants";
-import { useRouter } from "next/router";
-
-// export async function getServerSideProps() {
-//   const rounter = useRouter();
-//   const id = rounter.query;
-//   const jsondata = await fetch("http://localhost:8080/article" + { id }).then(
-//     (r) => r.json()
-//   );
-//   return {
-//     props: { jsondata },
-//   };
-// }
 
 export default function Test({ data }) {
-  return <h1>{data.title}</h1>;
+  return (
+    <>
+      <Header />
+      <div className="mx-auto my-10 w-11/12 rounded-2xl border-4 border-black bg-chgray px-7 py-10">
+        <h2>
+          <span className="text-5xl font-black text-red-600">{data.title}</span>
+        </h2>
+        <ul className="my-10 ml-7  list-decimal text-2xl">
+          <li className="font-bold">
+            名前：
+            <span className="font-bold text-green-700">
+              {data.nickname}
+            </span>{" "}
+            {data.created}
+          </li>
+          <dd className="my-2">{data.content}</dd>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export async function getServerSideProps({ query }) {
